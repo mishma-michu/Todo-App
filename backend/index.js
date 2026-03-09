cd6a7058443620ee86147631f7ef42a5270d93cf
mishma-michu <mishmabenny@gmail.com>
2026-03-09 15:58:15 +0530

    add task routes

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
 app.get('/', (req, res) => {
   res.send('API running');
 });
